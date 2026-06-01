6df839b95ef070f2ca4159d1baca43035d9d2298
CTapia10 <cristianalejandro_t@hotmail.com>
2026-05-31 21:40:14 -0300

    Fix Test file para desplegar proyecto local desde cmd

--- src/test/java/com/java/spring/zerotoheroapp/ZerotoheroappApplicationTests.java
+++ src/test/java/com/java/spring/zerotoheroapp/ZerotoheroappApplicationTests.java
@@ -2,8 +2,9 @@ package com.java.spring.zerotoheroapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.proyecto.spring.zerotoheroapp.ZerotoheroappApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = ZerotoheroappApplication.class)
 class ZerotoheroappApplicationTests {
 
 	@Test
