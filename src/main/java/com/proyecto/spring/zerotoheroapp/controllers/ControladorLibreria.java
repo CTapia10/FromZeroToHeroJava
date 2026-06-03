1c28c5c51d76d3b70d6acde4f71b2ae64b542a6b
CTapia10 <cristianalejandro_t@hotmail.com>
2026-06-03 00:36:44 -0300

    Models y Repository implementacion ejemplo libros

--- src/main/java/com/proyecto/spring/zerotoheroapp/controllers/ControladorLibreria.java
+++ src/main/java/com/proyecto/spring/zerotoheroapp/controllers/ControladorLibreria.java
@@ -3,9 +3,18 @@ package com.proyecto.spring.zerotoheroapp.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyecto.spring.zerotoheroapp.models.Libros;
+import com.proyecto.spring.zerotoheroapp.repository.RepoLibros;
 import com.proyecto.spring.zerotoheroapp.services.ServicioLibro;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @RestController
@@ -13,9 +22,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class ControladorLibreria {
 
     private final ServicioLibro libroServicio;
+    private final RepoLibros libroRepositorio;
 
-    public ControladorLibreria(ServicioLibro libroServicio) {
+    public ControladorLibreria(ServicioLibro libroServicio, RepoLibros libroRepositorio) {
         this.libroServicio = libroServicio;
+        this.libroRepositorio = libroRepositorio;
     }
 
     @GetMapping("/{titulo}")
@@ -23,4 +34,19 @@ public class ControladorLibreria {
         return libroServicio.buscaLibro(titulo);
     }
     
+    @GetMapping("/todos")
+    public List<Libros>dimeTodos(){
+        return libroRepositorio.findAll();
+    }
+    
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Libros>buscaId(@PathVariable Long id) {
+        return libroRepositorio.BuscaId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    
+    }
+    
+
+
 }
